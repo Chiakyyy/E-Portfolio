dfea7a5a42139399a2b26a0923fae1e7eec35a3f
Chiakyyy <agouriane.amr@gmail.com>
2025-10-18 16:53:11 +0100

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change 'username' and 'repo' to your GitHub username and repository
-const repo = process.env.REPO_NAME || 'e-portfolio-react';
+const repo = 'E-Portfolio';
 
 export default defineConfig({
   plugins: [react()],
